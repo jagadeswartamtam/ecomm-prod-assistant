55301ac9a4b54e2ab61db2390b08dce275ce63c2
jagadeswar <jagadeswartamtam@gmail.com>
2025-09-06 13:26:57 +0530

    my project initialization is done

---
+++ get_lib_versions.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+packages=[
+    "langchain",
+    "python-dotenv",
+    "langchain_core"
+]
+for pack in packages:
+    try:
+        versions=importlib.metadata.version(pack)
+        print(f"{pack}=={versions}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pack} (not installed)")
